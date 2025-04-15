5ca4a450781bf301ca96a3a8905210970e1e5b56
KodaEd <edrickkoda@gmail.com>
2025-04-16 01:02:34 +1000

    Added more metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,13 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "~/Edrick-Koda/Portfolio",
   description: "KodaSystems/7.7 Server at www.edrickkoda.com Port 80",
+  openGraph: {
+    title: "~/Edrick-Koda/Portfolio",
+    type: "website",
+    url: "https://www.edrickkoda.com",
+    images: "https://www.edrickkoda.com/og-image.png",
+    description: "KodaSystems/7.7 Server at www.edrickkoda.com Port 80",
+  },
 };
 
 export default function RootLayout({
